3f45320311ba1c493ae94964432e454da5ac95e6
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-11-02 19:41:18 -0400

    I hate gIsHappy

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,9 @@
 package io.zipcoder;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @author tariq
  */
@@ -11,17 +14,19 @@ public class StringsAndThings {
      * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
+    public Integer countYZ(String input) {
+        int i = 0;
         int result = 0;
         String[] word = input.split(" ");
-        for (int i = 0; i < word.length ; i++) {
-            String n = String.valueOf(word[i].charAt(word[i].length()-1));
-            if(n.equalsIgnoreCase("y") || n.equalsIgnoreCase("z")) {
+        for (String n : word) {
+            n = String.valueOf(word[i].charAt(word[i].length() - 1));
+            if (n.equalsIgnoreCase("y") || n.equalsIgnoreCase("z")) {
                 result++;
             }
+            i++;
         }
         return result;
     }
@@ -30,19 +35,19 @@ public class StringsAndThings {
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
      * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
+     * <p>
      * example : removeString("Hello there", "llo") // Should return "He there"
-     *           removeString("Hello there", "e") //  Should return "Hllo thr"
-     *           removeString("Hello there", "x") // Should return "Hello there"
+     * removeString("Hello there", "e") //  Should return "Hllo thr"
+     * removeString("Hello there", "x") // Should return "Hello there"
      */
-    public String removeString(String base, String remove){
+    public String removeString(String base, String remove) {
         String result = "";
         String[] temp = base.split(remove);
         for (int i = 0; i < temp.length; i++) {
-            if(temp[i]==remove) {
-                temp[i]="";
+            if (temp[i] == remove) {
+                temp[i] = "";
             } else {
-                result+=temp[i];
+                result += temp[i];
             }
         }
         return result;
@@ -51,35 +56,56 @@ public class StringsAndThings {
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
      * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
+     * <p>
      * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
-     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
-     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
+     * containsEqualNumberOfIsAndNot("This is notnot") // Should return true
+     * containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+        int i = 0, isN = 0, notN = 0;
+        String[] splitInput = input.split("((?= |is|not)|(?<= |is|not))");
+        for (String storage : splitInput) {
+            storage = splitInput[i];
+            if (storage.equalsIgnoreCase("is")) {
+                isN++;
+            }
+            if (storage.equalsIgnoreCase("not")) {
+                notN++;
+            }
+            i++;
+        }
+        return isN == notN;
     }
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
      * Return true if all the g's in the given string are happy.
      * example : gHappy("xxggxx") // Should return  true
-     *           gHappy("xxgxx") // Should return  false
-     *           gHappy("xxggyygxx") // Should return  false
+     * gHappy("xxgxx") // Should return  false
+     * gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
-        return null;
+    public Boolean gIsHappy(String input) {
+        input = input.replaceAll("gg", "1");
+        if(input.contains("g")) {
+            return false;
+        }
+        return true;
     }
-
-
-    /**
-     * We'll say that a "triple" in a string is a char appearing three times in a row.
-     * Return the number of triples in the given string. The triples may overlap.
-     * example :  countTriple("abcXXXabc") // Should return 1
-     *            countTriple("xxxabyyyycd") // Should return 3
-     *            countTriple("a") // Should return 0
-     */
-    public Integer countTriple(String input){
-        return null;
-    }
-}
+            /**
+             * We'll say that a "triple" in a string is a char appearing three times in a row.
+             * Return the number of triples in the given string. The triples may overlap.
+             * example :  countTriple("abcXXXabc") // Should return 1
+             *            countTriple("xxxabyyyycd") // Should return 3
+             *            countTriple("a") // Should return 0
+             */
+            public Integer countTriple (String input){
+                int n = 0;
+                for(int i = 1; i < input.length()-1; i++) {
+                    if(input.charAt(i-1) == input.charAt(i)
+                            && input.charAt(i) == input.charAt(i+1)) {
+                        n++;
+                    }
+                }
+                return n;
+            }
+        }
